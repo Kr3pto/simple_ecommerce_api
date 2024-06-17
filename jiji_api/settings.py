4db7c5c3cf60ee646e45930af3270411db34323e
Macbeth <fiagbenyamackbeth@gmail.com>
2024-06-17 01:26:49 +0000

    final commit

--- jiji_api/settings.py
+++ jiji_api/settings.py
@@ -25,11 +25,7 @@ SECRET_KEY = 'django-insecure-sp!ybm6yu$&d9$8wwvr7ls6-f)yx6)go(25@l4a1f9i$wr2o8#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'https://simple-ecommerce-api-1.onrender.com',
-    'simple-ecommerce-api-1.onrender.com'
-]
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
